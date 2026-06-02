3d1c6a2f7a7a12fbb7735e9b71818668bdff9d1b
Shashank Mishra <shashankmishra@Mac.lan>
2026-06-02 22:09:08 +0530

    feat: Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,10 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProtectedRoute from './hooks/ProtectedRoute';
-import { ROUTES } from './constants/routes';
-
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
-import HoldingsPage from './pages/HoldingsPage';
-import ProfilePage from './pages/ProfilePage';
-import NotFoundPage from './pages/NotFoundPage';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRoutes } from './routes';
 
 function App() {
-  const { isAuthenticated } = useSelector((state) => state.auth);
-
   return (
     <Router>
-      <Routes>
-        <Route path={ROUTES.LOGIN} element={isAuthenticated ? <Navigate to={ROUTES.DASHBOARD} replace /> : <LoginPage />} />
-        <Route path={ROUTES.REGISTER} element={isAuthenticated ? <Navigate to={ROUTES.DASHBOARD} replace /> : <RegisterPage />} />
-
-        <Route
-          path={ROUTES.DASHBOARD}
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={ROUTES.HOLDINGS}
-          element={
-            <ProtectedRoute>
-              <HoldingsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={ROUTES.PROFILE}
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path={ROUTES.ROOT} element={<Navigate to={isAuthenticated ? ROUTES.DASHBOARD : ROUTES.LOGIN} replace />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 }
